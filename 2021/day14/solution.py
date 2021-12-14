839e99be90ed15c3aee2131644e70e9b51ccdd0c
Christoph Lahner <clahner@aptean.com>
2021-12-14 08:56:52 +0100

    Add solution for day 14 part 2

--- 2021/day14/solution.py
+++ 2021/day14/solution.py
@@ -1,29 +1,32 @@
+from collections import defaultdict
 from typing import Counter
 
-def substitute(template, instructions):
-    i = 0
-    while i < len(template) - 1:
-        insertion = instructions[template[i:i+2]]
-        template = template[:i+1] + insertion + template[i+1:]
+def get_score(template, instructions, cnt):
+    pair_counts = Counter(x + y for x, y in zip(template, template[1:]))
+    letter_counts = Counter(template)
 
-        i += 2
+    for _ in range(cnt):
+        new_pair_counts = defaultdict(int)
+
+        for pair, cnt in pair_counts.items():
+            insertion = instructions[pair]
+            new_pair_counts[pair[0] + insertion] += cnt
+            new_pair_counts[insertion + pair[1]] += cnt
+            letter_counts[insertion] += cnt
+        
+        pair_counts = new_pair_counts
     
-    return template
+    count_vals = letter_counts.values()
 
-def get_score(template, instructions):
-    for _ in range(10):
-        template = substitute(template, instructions)
-    
-    c = Counter(template).most_common()
-    return c[0][1] - c[-1][1]
+    return max(count_vals) - min(count_vals)
 
-#with open("input.txt", "r") as f:
-with open("test-input.txt", "r") as f:
+with open("input.txt", "r") as f:
+#with open("test-input.txt", "r") as f:
     template = f.readline().rstrip()
     f.readline()
     instructions = dict([l.rstrip().split(" -> ") for l in f.readlines()])
 
-solution1 = get_score(template, instructions)
-solution2 = 0
+solution1 = get_score(template, instructions, 10)
+solution2 = get_score(template, instructions, 40)
 
 print(solution1, solution2)
