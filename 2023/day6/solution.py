ad3a6b890dfaab3ca3fe8c0900b0851ac9ada609
Christoph Lahner <clahner@aptean.com>
2023-12-07 07:14:57 +0100

    Optimize day6 solution

--- 2023/day6/solution.py
+++ 2023/day6/solution.py
@@ -1,18 +1,19 @@
 
-solution2 = 0
-
 from operator import mul
 from functools import reduce
+from math import floor, ceil, sqrt
 
 def get_wins(total_time: int, record: int)->int:
-    count = 0
+    # Calculated with the following approach:
+    # (1) travelled time = race time - button press time
+    # (2) distance = travelled time * button press time
+    # insert (1) into (2) => (3) distance = (race time - button press time) * button press time
+    # you can then get a quadratic formula for the button press time(s)
+    # the difference between this range is the count of wins
+    b1 = floor((total_time + sqrt(pow(total_time, 2) - 4 * record))/2)
+    b2 = ceil((total_time - sqrt(pow(total_time, 2) - 4 * record))/2)
 
-    for hold_time in range(1, total_time):
-        distance = hold_time * (total_time - hold_time)
-        if distance > record:
-            count += 1
-
-    return count
+    return b1-b2+1
 
 def solve1(times: list[int], records: list[int])->int:
     winning_counts = [get_wins(time, record) for time, record in zip(times, records)]
