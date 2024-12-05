21dab607dc1a767d68e6e2642740577a5086c518
Christoph Lahner <clahner@aptean.com>
2024-12-05 08:54:46 +0100

    Refactor solution for day5 2024 to use sorting

--- solutions/year2024/day5/solution.py
+++ solutions/year2024/day5/solution.py
@@ -1,43 +1,28 @@
 from framework.solution_base import SolutionBase
+from functools import cmp_to_key
 
 class Solution(SolutionBase):
-    def parse_input(self, lines: list[str])->tuple[list[tuple[int, int]], list[list[int]]]:
-        empty_idx = lines.index('')
-        rules = [tuple(map(int, l.split('|'))) for l in lines[:empty_idx]]
-        updates = [list(map(int, l.split(','))) for l in lines[empty_idx+1:]]
-
-        return rules, updates
-
-    def get_middle(self, update: list[int])->int:
-        return update[len(update)//2]
     
-    def is_valid(self, update: list[int], rules: list[tuple[int, int]])->bool:
-        for rule in rules:
-            idx_a = update.index(rule[0]) if rule[0] in update else -1
-            idx_b = update.index(rule[1]) if rule[1] in update else -1
+    def solve_core(self, lines: list[str], use_valid: bool):
+        empty_line = lines.index('')
+        rules = lines[:empty_line]
+        updates = lines[empty_line+1:]
 
-            if idx_a >= 0 and idx_b >= 0 and idx_b < idx_a:
-                return False
+        # returns -1 if in rules and 1 if not so that x is before y
+        cmp = cmp_to_key(lambda x,y: 1-2*(f'{x}|{y}' in rules))
+    
+        result = 0
+        for u in updates:
+            update = u.split(',')
+            sorted_u = sorted(update, key=cmp)
 
-        return True
-
-    def order(self, update: list[int], rules: list[tuple[int, int]])->list[int]:
-        for rule in rules:
-            idx_a = update.index(rule[0]) if rule[0] in update else -1
-            idx_b = update.index(rule[1]) if rule[1] in update else -1
-
-            if idx_a >= 0 and idx_b >= 0 and idx_b < idx_a:
-                update[idx_a], update[idx_b] = update[idx_b], update[idx_a]
-                return self.order(update, rules)
-
-        return update
+            is_valid = sorted_u == update
+            result += int(sorted_u[len(sorted_u)//2]) if use_valid == is_valid else 0
+        
+        return result
 
     def solve_1(self, lines: list[str]):
-        rules, updates = self.parse_input(lines)
-
-        return sum([self.get_middle(u) for u in updates if self.is_valid(u, rules)])
+        return self.solve_core(lines, True)
     
     def solve_2(self, lines: list[str]):
-        rules, updates = self.parse_input(lines)
-        
-        return sum([self.get_middle(self.order(u, rules)) for u in updates if not self.is_valid(u, rules)])
+        return self.solve_core(lines, False)
