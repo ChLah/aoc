6d1c940ffbe62cc0a9d7888297556a93a48172da
Christoph Lahner <clahner@aptean.com>
2025-12-02 08:40:10 +0100

    Use regex to speed up solution

--- solutions/year2025/day2/solution.py
+++ solutions/year2025/day2/solution.py
@@ -1,25 +1,11 @@
 from framework.solution_base import SolutionBase
+import re
 
 class Solution(SolutionBase):
-    def segmentize_str(self, str: str, segment_length: int) -> list[str]:
-        return [str[i:i + segment_length] for i in range(0, len(str), segment_length)]
-
     def is_invalid(self, number: int, dynamic_lengths: bool) -> bool:
-        if number < 10:
-            return False
-        
-        as_str = str(number)
-
-        if dynamic_lengths:
-            for length in range(1, len(as_str) // 2 + 1):
-                segments = self.segmentize_str(as_str, length)
-                if segments.count(segments[0]) == len(segments):
-                    return True
-            return False
-
-        else:
-            segments = self.segmentize_str(as_str, len(as_str) // 2)
-            return segments.count(segments[0]) == len(segments)
+        regexp = r"^(\d+)\1+$" if dynamic_lengths else r"^(\d+)\1$"
+        result = re.search(regexp, str(number))
+        return result is not None
     
     def solve_core(self, line: str, dynamic_lengths: bool)->int:
         ranges = [r.split("-") for r in line.split(",")]
